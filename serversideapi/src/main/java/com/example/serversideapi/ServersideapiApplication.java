4119d080c4d7746d34da6bb0ffe5b0596894e39a
Kasra Alizadeh <k.alizadeh1997@gmail.com>
2022-06-30 20:38:21 +0430

    Project created

---
+++ serversideapi/src/main/java/com/example/serversideapi/ServersideapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.serversideapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServersideapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServersideapiApplication.class, args);
+    }
+
+}
