b6e59a4325f5484233d4b26b415112baed119d5e
Kasra Alizadeh <k.alizadeh1997@gmail.com>
2022-07-02 17:28:48 +0430

    Comments added

--- serversideapi/src/main/java/com/example/serversideapi/GreetingController.java
+++ serversideapi/src/main/java/com/example/serversideapi/GreetingController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+// create structure of controller
 @RestController
 public class GreetingController {
 
